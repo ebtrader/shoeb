af00f45791bffaed4a7a00458186f089db7d905e
ebtrader <jsiddique@gmail.com>
2021-09-07 17:57:54 -0400

    Initial Commit

--- test_finta_move.py
+++ test_finta_move.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 import pandas as pd
-from finta import TA
+# from finta import TA
 
 ticker = 'NQ=F'
 data = yf.download(ticker, start="2020-01-01", end="2021-09-03")
@@ -9,8 +9,22 @@ df = pd.DataFrame(data)
 df.columns= df.columns.str.lower()
 # print(df)
 
-df['simple_moving_avg'] = TA.SMA(df, 9)
+def SMA(ohlc = df, period: int = 41, column: str = "close"):
+    """
+    Simple moving average - rolling mean in pandas lingo. Also known as 'MA'.
+    The simple moving average (SMA) is the most basic of the moving averages used for trading.
+    """
+
+    return pd.Series(
+        ohlc[column].rolling(window=period).mean(),
+        name="{0} period SMA".format(period),
+    )
+
+df['simple_moving_avg'] = SMA(df)
 print(df)
+# df['hma'] = TA.HMA(df, 9)
+# print(df)
+
 
 
 
